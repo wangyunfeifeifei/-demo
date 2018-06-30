9aebd8740b638285b209a722b66f0063ef0b5de9
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-06-30 14:52:14 +0800

    完成自定义滚动组件的设计

--- frontEnd/每周特效/jquery自定义滚动条/static/CusScrollBar.js
+++ frontEnd/每周特效/jquery自定义滚动条/static/CusScrollBar.js
@@ -9,13 +9,20 @@
                 scrollDir:  'y', // 滚动方向
                 contentSelector: '', // 滚动内容区的选择器
                 barSelector: '', // 滚动条的选择器
-                sliderSelector: '' // 滚动滑块的选择器
+                sliderSelector: '', // 滚动滑块的选择器
+                tabItemSelector: '.tab-item',
+                tabActiveClass: 'tab-active',
+                sectionSelector: '.anchor',
+                wheelStep: 20, // 滚轮的步长，滚动移动距离
             }
             Object.assign(self.options, options || {})
             self.__initDomEvent__()
+                .__calculateHeight__()
                 .__initSliderHeight__()
                 .__initSliderDragEvent__()
                 .__bindContentScroll__()
+                .__bindMouseScroll__()
+                .__bindTabEvent__()
         },
         /**
          * 初始化DOM引用
@@ -25,6 +32,7 @@
             this.$content = $(options.contentSelector)
             this.$slider = $(options.sliderSelector)
             this.$bar = options.barSelector ? $(options.barSelector) : this.$slider.parent()
+            this.$tabItem = $(options.tabItemSelector)
             this.$doc = $(doc)
             return this
         },
@@ -68,12 +76,57 @@
                 if (sliderEl) {
                     sliderEl.style.top = `${self.getSliderPosition()}px`
                 }
+                for(let i=0; i<this.sectionHeight.length; i++) {
+                    let height1 = this.sectionHeight[i]
+                    let height2 = this.sectionHeight[i+1]
+                    if (!height2 || this.$content[0].scrollTop > height1 && this.$content[0].scrollTop < height2) {
+                        this.changeTabActive(i)
+                        break;
+                    }
+                }
             })
             return this
         },
         // 监听鼠标滚轮事件
         __bindMouseScroll__() {
-
+            this.$content.on("mousewheel DOMMouseScroll", (e) => {
+                e.preventDefault()
+                let originEv = e.originalEvent
+                let wheelRange = originEv.wheelDelta ? -originEv.wheelDelta / 120 : (originEv.detail || 0) / 3;
+                this.scrollTo(this.$content[0].scrollTop + wheelRange * this.options.wheelStep)
+            })
+            return this
+        },
+        __bindTabEvent__() {
+            let self = this
+            self.$tabItem.on('click', 'a', function(e) {
+                e.preventDefault()
+                let index = $(this).parent().index()
+                let top = Math.min($($(this).attr('href')).position().top + self.$content.scrollTop(), self.getMaxScrollPosition()) 
+                self.$content.stop().animate({
+                    scrollTop: top
+                }, {
+                    duration: 500,
+                    easing: 'easeInOutQuint'
+                })
+            })
+            return this
+        },
+        __calculateHeight__() {
+            let self = this
+            self.sectionHeight = []
+            let sectionHeight = self.sectionHeight
+            let height = 0
+            sectionHeight.push(height)
+            $(self.options.sectionSelector).each(function() {
+                height += $(this).height()
+                sectionHeight.push(height)
+            })
+            return this
+        },
+        changeTabActive(index) {
+            let activeClass = this.options.tabActiveClass
+            this.$tabItem.eq(index).addClass(activeClass).siblings().removeClass(activeClass)
         },
         // 计算滑块当前应该的位置
         getSliderPosition() {
