f5950ae65a6aec6cd8cedbd231729627b9ad033a
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-08-05 19:56:56 +0800

    添加myblog项目

--- backend/helloExpress/index.js
+++ backend/helloExpress/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users')
+const path = require('path');
 
-app.get('/', function (req, res) {
-    res.send('hello, express');
-});
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+// 访问如 localhost:3000/users/wang 时返回 ‘hello,wang’
+app.use('/users',usersRouter);
 
 app.listen(3000, () => {
     console.log('app is start at port 3000')
