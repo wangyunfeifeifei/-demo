07912fa8c696d81b693d0526f506d5e42702bae4
wangyunfeifeifei <wangyunfeiafei@outlook.com>
2018-08-05 19:37:25 +0800

    添加hello express

---
+++ backend/helloExpress/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('hello, express');
+});
+
+app.listen(3000, () => {
+    console.log('app is start at port 3000')
+})
